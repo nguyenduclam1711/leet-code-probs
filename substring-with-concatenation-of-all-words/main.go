69eda47ecdca4bdb7838b47ec93718a00bb992a8
Lam LUL <nguyenduclam1711@gmail.com>
2024-05-07 10:12:36 +0700

    Add solution for Substring with Concatenation of All Words

--- substring-with-concatenation-of-all-words/main.go
+++ substring-with-concatenation-of-all-words/main.go
@@ -1,5 +1,6 @@
 package substringwithconcatenationofallwords
 
+// using hash map and clone hash map for checking
 func findSubstring(s string, words []string) []int {
 	hashMap := map[string]int{}
 	wordLen := len(words[0])
@@ -25,6 +26,42 @@ func findSubstring(s string, words []string) []int {
 			currHashMap[currWord]--
 		}
 		if j == i+windowLength {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+// still use hashmap but not clone hashmap from the beginning => reduce the space uses
+func findSubstring2(s string, words []string) []int {
+	hashMap := map[string]int{}
+	for _, word := range words {
+		hashMap[word]++
+	}
+	wordLen := len(words[0])
+	windowLen := len(words) * wordLen
+	res := []int{}
+
+	for i := 0; i <= len(s)-windowLen; i++ {
+		seenWords := map[string]int{}
+		count := 0
+
+		for j := i; j < i+windowLen; j += wordLen {
+			currWord := s[j : j+wordLen]
+			if _, e := hashMap[currWord]; e {
+				seenWords[currWord]++
+				count++
+
+				if seenWords[currWord] > hashMap[currWord] {
+					count = 0
+					break
+				}
+			} else {
+				count = 0
+				break
+			}
+		}
+		if count == len(words) {
 			res = append(res, i)
 		}
 	}
--- substring-with-concatenation-of-all-words/main_test.go
+++ substring-with-concatenation-of-all-words/main_test.go
@@ -7,22 +7,45 @@ import (
 )
 
 func TestFindSubstring(t *testing.T) {
-	// test1 := findSubstring("barfoothefoobarman", []string{"foo", "bar"})
-	// if !reflect.DeepEqual([]int{0, 9}, test1) {
-	// 	fmt.Println("test1", test1)
-	// 	t.FailNow()
-	// }
-	// test2 := findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "word"})
-	// if !reflect.DeepEqual([]int{}, test2) {
-	// 	fmt.Println("test2", test2)
-	// 	t.FailNow()
-	// }
-	// test3 := findSubstring("barfoofoobarthefoobarman", []string{"bar", "foo", "the"})
-	// if !reflect.DeepEqual([]int{6, 9, 12}, test3) {
-	// 	fmt.Println("test3", test3)
-	// 	t.FailNow()
-	// }
+	test1 := findSubstring("barfoothefoobarman", []string{"foo", "bar"})
+	if !reflect.DeepEqual([]int{0, 9}, test1) {
+		fmt.Println("test1", test1)
+		t.FailNow()
+	}
+	test2 := findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "word"})
+	if !reflect.DeepEqual([]int{}, test2) {
+		fmt.Println("test2", test2)
+		t.FailNow()
+	}
+	test3 := findSubstring("barfoofoobarthefoobarman", []string{"bar", "foo", "the"})
+	if !reflect.DeepEqual([]int{6, 9, 12}, test3) {
+		fmt.Println("test3", test3)
+		t.FailNow()
+	}
 	test4 := findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"})
+	if !reflect.DeepEqual([]int{8}, test4) {
+		fmt.Println("test4", test4)
+		t.FailNow()
+	}
+}
+
+func TestFindSubstring2(t *testing.T) {
+	test1 := findSubstring2("barfoothefoobarman", []string{"foo", "bar"})
+	if !reflect.DeepEqual([]int{0, 9}, test1) {
+		fmt.Println("test1", test1)
+		t.FailNow()
+	}
+	test2 := findSubstring2("wordgoodgoodgoodbestword", []string{"word", "good", "best", "word"})
+	if !reflect.DeepEqual([]int{}, test2) {
+		fmt.Println("test2", test2)
+		t.FailNow()
+	}
+	test3 := findSubstring2("barfoofoobarthefoobarman", []string{"bar", "foo", "the"})
+	if !reflect.DeepEqual([]int{6, 9, 12}, test3) {
+		fmt.Println("test3", test3)
+		t.FailNow()
+	}
+	test4 := findSubstring2("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"})
 	if !reflect.DeepEqual([]int{8}, test4) {
 		fmt.Println("test4", test4)
 		t.FailNow()
