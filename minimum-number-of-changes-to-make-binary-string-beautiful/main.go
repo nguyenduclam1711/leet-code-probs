097250d9fad267866bf6598584f053ab7a0c11f1
Lam LUL <nguyenduclam1711@gmail.com>
2024-11-06 14:51:59 +0700

    Minimum Number of Changes to Make Binary String Beautiful

---
+++ minimum-number-of-changes-to-make-binary-string-beautiful/main.go
@@ -0,0 +1,11 @@
+package minimumnumberofchangestomakebinarystringbeautiful
+
+func minChanges(s string) int {
+	res := 0
+	for i := 0; i < len(s); i += 2 {
+		if s[i] != s[i+1] {
+			res++
+		}
+	}
+	return res
+}
---
+++ minimum-number-of-changes-to-make-binary-string-beautiful/readme.md
@@ -0,0 +1,1 @@
+# [Minimum Number of Changes to Make Binary String Beautiful](https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful)
